6764c042e1fba30a4a3017cb300ebb0fee9e11b6
kesylo <32457792+kesylo@users.noreply.github.com>
2018-05-21 01:14:48 +0200

    1

---
+++ Assets/Arbre.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arbre : MonoBehaviour {
+    private Mesh mesh;
+    private Texture ecorce;
+    private Texture feuilles;
+    private Vector2 position;
+    private double hauteur;
+    private double epaisseur;
+    private Color couleurEcorce;
+    private Color couleurFeuille;
+
+}
